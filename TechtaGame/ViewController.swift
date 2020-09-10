1ca09a735c61018ab54d54785af9b194937257d5
lifeistech <kaairiinn0913@yahoo.co.jp>
2020-09-10 14:45:13 +0900

    Initial Commit

---
+++ TechtaGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechtaGame
+//
+//  Created by 井戸海里 on 2020/09/10.
+//  Copyright © 2020 IdoUmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TechtaGameTests/TechtaGameTests.swift
@@ -0,0 +1,34 @@
+//
+//  TechtaGameTests.swift
+//  TechtaGameTests
+//
+//  Created by 井戸海里 on 2020/09/10.
+//  Copyright © 2020 IdoUmi. All rights reserved.
+//
+
+import XCTest
+@testable import TechtaGame
+
+class TechtaGameTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
